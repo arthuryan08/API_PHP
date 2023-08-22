d672118d3f50c04a5bfbddbcb8ad26b36ee4bbef
Arthur Yan <arthuryan08@gmail.com>
2023-08-22 13:34:16 -0300

    insert values done

--- api.php
+++ api.php
@@ -2,7 +2,7 @@
 require('./config.php');
 
 // Endpoint para obter os detalhes de todas as campanhas
-if ($_SERVER['REQUEST_URI'] === '/backendtest/api.php/all') {
+if ($_SERVER['REQUEST_URI'] === '/backendtest/api.php/allcampaings') {
     if($_SERVER['REQUEST_METHOD'] === 'GET'){
         $sql = $pdo->query("SELECT * FROM campaigns");
         if($sql->rowCount() > 0) {
@@ -25,7 +25,8 @@ if ($_SERVER['REQUEST_URI'] === '/backendtest/api.php/all') {
     }
 }
 
-if (strpos($_SERVER['REQUEST_URI'], '/backendtest/api.php/get') !== false) {
+// Endpoint para obter os detalhes de uma campanha especifica
+if (strpos($_SERVER['REQUEST_URI'], '/backendtest/api.php/getcampaing') !== false) {
     if($_SERVER['REQUEST_METHOD'] === 'GET'){
         $id = filter_input(INPUT_GET, 'id');
         if($id) {
@@ -40,6 +41,7 @@ if (strpos($_SERVER['REQUEST_URI'], '/backendtest/api.php/get') !== false) {
                     'id' => $data['id'],
                     'name' => $data['name'],
                     'campaingBudget' => $data['campaignbudget'],
+                    'campaignpublic' => $data['campaignpublic'],
                     'startDate' => $data['startdate'],
                     'endDate' => $data['enddate'],
                     'urlMedia' => $data['media'],
@@ -55,5 +57,61 @@ if (strpos($_SERVER['REQUEST_URI'], '/backendtest/api.php/get') !== false) {
         $response['error'] = 'Method Not Allowed';
     }
 };
+
+// Endpoint para inserir uma nova campanha
+if ($_SERVER['REQUEST_URI'] === '/backendtest/api.php/campaigns') {
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $requiredFields = ['name', 'campaignbudget', 'campaignpublic', 'startdate', 'enddate'];
+        $filteredData = filterInputFields(INPUT_POST, $requiredFields);
+
+        if ($filteredData) {
+            insertCampaignIntoDatabase($pdo, $filteredData);
+            $id = $pdo->lastInsertId();
+            $response['result'] = createResponseArray($id, $filteredData);
+        } else {
+            $response['error'] = 'Fields Not Sended or Missing';
+        }
+    } else {
+        $response['error'] = 'Method Not Allowed';
+    }
+}
+
+function filterInputFields($inputType, $fields) {
+    $filteredData = [];
+
+    foreach ($fields as $field) {
+        $value = filter_input($inputType, $field);
+        if (!$value) {
+            return false;
+        }
+        $filteredData[$field] = $value;
+    }
+
+    return $filteredData;
+}
+
+function insertCampaignIntoDatabase($pdo, $data) {
+    $sql = $pdo->prepare
+    ('INSERT INTO campaigns (name, campaignbudget, campaignpublic, startdate, enddate) 
+    VALUES (:name, :campaignbudget, :campaignpublic, :startdate, :enddate)');
+    
+    foreach ($data as $field => $value) {
+        $sql->bindValue(':' . $field, $value);
+    }
+    
+    $sql->execute();
+}
+
+function createResponseArray($id, $data) {
+    return [
+        'id' => $id,
+        'name' => $data['name'],
+        'campaignBudget' => $data['campaignbudget'],
+        'campaignpublic' => $data['campaignpublic'],
+        'startDate' => $data['startdate'],
+        'endDate' => $data['enddate'],
+        'status' => 'active',
+    ];
+}
 
 require('./return.php');
