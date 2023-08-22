8f2afec77067d69890e18f5eccb1a315fedf9f5a
Arthur Yan <arthuryan08@gmail.com>
2023-08-22 11:57:06 -0300

    get all data done

--- api.php
+++ api.php
@@ -1,6 +1,26 @@
 <?php
 require('./config.php');
 
-$response['pong'] = true;
+if ($_SERVER['REQUEST_METHOD'] === 'GET' && $_SERVER['REQUEST_URI'] === '/backendtest/api.php/getall') {
+
+    $sql = $pdo->query("SELECT * FROM campaigns");
+    if($sql->rowCount() > 0) {
+        $data = $sql->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach($data as $item){
+            $response['result'][] = [
+                'id' => $item['id'],
+                'name' => $item['name'],
+                'campaingBudget' => $item['campaignbudget'],
+                'startDate' => $item['startdate'],
+                'endDate' => $item['enddate'],
+                'urlMedia' => $item['media'],
+                'status' => $item['status'],
+            ];
+        };
+    }
+} else {
+    $response['error'] = 'Method Not Allowed';
+}
 
 require('./return.php');
